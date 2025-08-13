28c7a0ea91231fb9269947995fe0f5647da55af9
Antônio Costa <antonioclaudiocostajr@gmail.com>
2025-08-13 16:48:27 -0300

    Colors added (#1)

---
+++ API Resquet/API Resquet/API_ResquetApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ResquetApp.swift
+//  API Resquet
+//
+//  Created by Antonio Costa on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct API_ResquetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
