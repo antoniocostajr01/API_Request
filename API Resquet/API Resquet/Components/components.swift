4855106ac20af41dd024850b6cdd3e541a7c2311
Sofia Leitão <sfleitao@inf.ufrgs.br>
2025-08-13 17:07:26 -0300

    chore: struct

---
+++ API Resquet/API Resquet/Components/components.swift
@@ -0,0 +1,18 @@
+//
+//  components.swift
+//  API Resquet
+//
+//  Created by sofia leitao on 13/08/25.
+//
+
+import SwiftUI
+
+struct components: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    components()
+}
