22d3661c66c17390f1828a88bd6e2369d168813e
Antônio Costa <antonioclaudiocostajr@gmail.com>
2025-08-18 16:18:09 -0300

    bug fix but not fix

--- API Resquet/API Resquet/Views/Home.swift
+++ API Resquet/API Resquet/Views/Home.swift
@@ -1,10 +1,6 @@
 import SwiftUI
 
 struct Home: View {
-<<<<<<< HEAD:API Resquet/API Resquet/Views/Home.swift
-    @StateObject private var vm = HomeViewModel()
-
-=======
     @StateObject private var vm = HomeViewModel(service: DummyJSONService())
     
     private let columns: [GridItem] = [
@@ -12,7 +8,6 @@ struct Home: View {
         GridItem(.flexible(), spacing: 12)
     ]
     
->>>>>>> main:API Resquet/API Resquet/Home.swift
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -26,23 +21,6 @@ struct Home: View {
                         .padding()
                 }
                 
-<<<<<<< HEAD:API Resquet/API Resquet/Views/Home.swift
-                if let deal = vm.deals.first {
-                    VStack(alignment: .leading, spacing: 8) {
-                        Text("Deals of the day")
-                            .font(.system(.title2, weight: .semibold))
-                            .foregroundStyle(.primary)
-
-                        ProductHorizontal(
-                            category: deal.category,
-                            title: deal.title,
-                            price: String(format: "%.2f", deal.price),
-                            imageURL: deal.thumbnail,
-                            onTap: { vm.selectedProduct = deal },
-                            isFavorite: false
-                        )
-                        .frame(width: 361, height: 176)
-=======
                 
                 VStack(alignment: .leading, spacing: 8) {
                     Text("Deals of the day")
@@ -56,14 +34,17 @@ struct Home: View {
                                     category: firstElement.category,
                                     title: firstElement.title,
                                     price: String(format: "%.2f", firstElement.price),
-                                    imageURL: firstElement.thumbnail
+                                    imageURL: firstElement.thumbnail, isFavorite: true
                                 )
                                 .frame(width: 361, height: 176)
+
                             }
+
                         }
                         .padding(.vertical, 4)
->>>>>>> main:API Resquet/API Resquet/Home.swift
                     }
+
+
                 }
                 .padding(.horizontal)
                 .padding(.top)
@@ -74,21 +55,13 @@ struct Home: View {
                         Text("Top picks")
                             .font(.system(.title2, weight: .semibold))
                             .foregroundStyle(.primary)
-<<<<<<< HEAD:API Resquet/API Resquet/Views/Home.swift
-
-                        LazyVGrid(columns: vm.columns, spacing: 12) {
-                            ForEach(vm.topPicks) { p in
-=======
                         
                         LazyVGrid(columns: columns, spacing: 12) {
                             ForEach(vm.products) { p in
->>>>>>> main:API Resquet/API Resquet/Home.swift
                                 ProductVertical(
                                     title: p.title,
                                     price: "US$" + String(format: "%.2f", p.price),
-                                    imageURL: p.thumbnail,
-                                    onTap: { vm.selectedProduct = p },
-                                    isFavorite: false
+                                    imageURL: p.thumbnail, isFavorite: true
                                 )
                             }
                         }
@@ -101,11 +74,6 @@ struct Home: View {
             .navigationTitle("Home")
         }
         .task { await vm.load() }
-        .sheet(item: $vm.selectedProduct) { product in
-            ProductDetail(product: product)
-                .presentationDragIndicator(.visible)
-        }
     }
 }
-
 
